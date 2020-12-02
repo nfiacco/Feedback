ef80a2cc3eff51f75e888c4fc16568e75bca08cb
Nick Fiacco <nicholas.r.fiacco.17@dartmouth.edu>
2020-12-02 01:55:26 -0500

    Setup database access through secrets

--- backend/go.mod
+++ backend/go.mod
@@ -3,8 +3,15 @@ module feedback
 go 1.15
 
 require (
+	cloud.google.com/go v0.72.0
+	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/gorilla/mux v1.8.0
+	golang.org/x/net v0.0.0-20201201195509-5d6afe98e0b7 // indirect
+	golang.org/x/oauth2 v0.0.0-20201109201403-9fd604954f58 // indirect
+	golang.org/x/sys v0.0.0-20201201145000-ef89a241ccb3 // indirect
 	google.golang.org/api v0.35.0
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20201201144952-b05cb90ed32e
 	gorm.io/driver/postgres v1.0.5
 	gorm.io/driver/sqlite v1.1.4 // indirect
 	gorm.io/gorm v1.20.7
--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -1,21 +1,19 @@
 package database
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func mustGetenv(k string) string {
-	v := os.Getenv(k)
-	if v == "" {
-		log.Fatalf("Warning: %s environment variable not set.\n", k)
-	}
-	return v
-}
+const DB_PWD_SECRET = "projects/621422061156/secrets/cloud-sql-feedback-password/versions/latest"
 
 func InitDatabase() (*gorm.DB, error) {
 	if isProd() {
@@ -39,11 +37,15 @@ func initDatabaseDev() (*gorm.DB, error) {
 
 func initDatabaseProd() (*gorm.DB, error) {
 	var (
-		dbUser                 = mustGetenv("DB_USER")                  // e.g. 'my-db-user'
-		dbPwd                  = mustGetenv("DB_PASS")                  // e.g. 'my-db-password'
-		instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
-		dbName                 = mustGetenv("DB_NAME")                  // e.g. 'my-database'
+		dbUser                 = mustGetenv("DB_USER")
+		instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME")
+		dbName                 = mustGetenv("DB_NAME")
 	)
+
+	dbPwd, err := fetchSecret(DB_PWD_SECRET)
+	if err != nil {
+		return nil, err
+	}
 
 	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
 	if !isSet {
@@ -51,7 +53,7 @@ func initDatabaseProd() (*gorm.DB, error) {
 	}
 
 	var dbURI string
-	dbURI = fmt.Sprintf("user=%s password=%s database=%s host=%s/%s", dbUser, dbPwd, dbName, socketDir, instanceConnectionName)
+	dbURI = fmt.Sprintf("user=%s password=%s database=%s host=%s/%s", dbUser, *dbPwd, dbName, socketDir, instanceConnectionName)
 
 	db, err := gorm.Open(postgres.Open(dbURI), &gorm.Config{})
 	if err != nil {
@@ -59,6 +61,34 @@ func initDatabaseProd() (*gorm.DB, error) {
 	}
 
 	return db, nil
+}
+
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Fatalf("Warning: %s environment variable not set.\n", k)
+	}
+	return v
+}
+
+func fetchSecret(name string) (*string, error) {
+	ctx := context.Background()
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create secretmanager client: %v", err)
+	}
+
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: name,
+	}
+
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to access secret version: %v", err)
+	}
+
+	secret := string(result.Payload.Data)
+	return &secret, nil
 }
 
 func isProd() bool {
