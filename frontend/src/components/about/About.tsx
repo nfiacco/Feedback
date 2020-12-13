ec6caadbaf4cec5cfec23826aaf617f023854d6a
Nick Fiacco <nicholas.r.fiacco.17@dartmouth.edu>
2020-12-13 02:07:35 -0500

    Update About page

--- frontend/src/components/about/About.tsx
+++ frontend/src/components/about/About.tsx
@@ -1,14 +1,19 @@
 import styles from "./about.m.css";
-import image from "./elmo.gif";
 
 export const About: React.FC = () => {
   return (
     <>
       <div className={styles.info}>
-        I made this for fun.
+        About Us
       </div>
       <div className={styles.body}>
-        <img className={styles.image} src={image} alt="loading..." />
+        We created Anonymous Feedback based on the belief that feedback is a gift, and critical to our personal development.
+        However, the reality is that giving and receiving feedback can be pretty awkward. By providing a mechanism
+        to give and receive anonymous feedback, we hope to make constant feedback a cultural norm.
+        <br/><br/>
+        We hope you enjoy the app, and leave us a review using *our* feedback link below:
+        <br/><br/>
+        TODO
       </div>
     </>
   );
